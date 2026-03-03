235d5d0bd70624e7d95b8869c2287174fc15c844
manjarii04 <mp4533@srmist.edu.in>
2026-03-03 09:27:44 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,3 +1,17 @@
+/**
+ * OOPSBannerApp prints OOPS using asterisks.
+ */
 public class OOPSBannerApp {
-	public static void main(Sring args[] )
-	System.out.println("OOPS");
+
+    /**
+     * Main method - entry point of the application.
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(" ***   ***   ***   ***** ");
+        System.out.println("*   * *   * *   *  *     ");
+        System.out.println("*   * *   * *   *  ***** ");
+        System.out.println("*   * *   * *          * ");
+        System.out.println(" ***   ***  *      ***** ");
+    }
+}
