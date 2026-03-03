457fbd7dd6a10b3a8d0dafa01b12a0ce4a5495e1
manjarii04 <mp4533@srmist.edu.in>
2026-03-03 09:34:21 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,3 +1,44 @@
+/**
+ * OOPSBannerApp prints OOPS in large ASCII banner format.
+ */
 public class OOPSBannerApp {
-	public static void main(Sring args[] )
-	System.out.println("OOPS");
+
+    /**
+     * Main method - entry point of the application.
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+        String O1_1 = "  *****  ";
+        String O1_2 = " *     * ";
+        String O1_3 = " *     * ";
+        String O1_4 = " *     * ";
+        String O1_5 = " *     * ";
+        String O1_6 = " *     * ";
+        String O1_7 = "  *****  ";
+
+        String P_1 =  " ******  ";
+        String P_2 =  " *     * ";
+        String P_3 =  " *     * ";
+        String P_4 =  " ******  ";
+        String P_5 =  " *       ";
+        String P_6 =  " *       ";
+        String P_7 =  " *       ";
+
+        String S_1 =  "  *****  ";
+        String S_2 =  " *     * ";
+        String S_3 =  " *       ";
+        String S_4 =  "  *****  ";
+        String S_5 =  "        *";
+        String S_6 =  " *     * ";
+        String S_7 =  "  *****  ";
+
+        System.out.println(O1_1 + " " + O1_1 + " " + P_1 + " " + S_1);
+        System.out.println(O1_2 + " " + O1_2 + " " + P_2 + " " + S_2);
+        System.out.println(O1_3 + " " + O1_3 + " " + P_3 + " " + S_3);
+        System.out.println(O1_4 + " " + O1_4 + " " + P_4 + " " + S_4);
+        System.out.println(O1_5 + " " + O1_5 + " " + P_5 + " " + S_5);
+        System.out.println(O1_6 + " " + O1_6 + " " + P_6 + " " + S_6);
+        System.out.println(O1_7 + " " + O1_7 + " " + P_7 + " " + S_7);
+    }
+}
